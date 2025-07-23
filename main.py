5a9ab75789396ee5118ddf2277dae1f1561615d4
stlgolfer <amigala@ucsd.edu>
2025-07-23 15:25:08 -0700

    Split redshift cuts

--- main.py
+++ main.py
@@ -31,6 +31,11 @@ stellar_types_dictionary = ["Main_Sequence_<=_0.7",
                                    "Binary_Star" ,
     "Not_a_Star!"]
 
+sensitivity_dictionary = {
+    'CE': 10,
+    'LVK': 1
+}
+
 # to obtain properties of ALL binaries simulated, do this:
 path = './Boesky_sims.h5'
 
@@ -65,21 +70,9 @@ print(np.shape(w_per_z_per_system))
 z_index = 0
 
 print('the redshift weights per system at z = ', redshifts[z_index], ' are given by' )
-w_z_index =w_per_z_per_system[:,z_index]
+# w_z_index =w_per_z_per_system[:,z_index]
 # want to sum up to a certain redshift. For now, sum up to redshift 1
-up_to_redshift = 1
-r_index = np.where(redshifts == up_to_redshift)
-if len(r_index) == 0:
-    raise "Redshift not found..consider changing your FastCosmicIntegration settings"
-r_index = r_index[0][0] # redshifts are monotone increasing so first result is ok
-w_z_summed = np.sum(
-    w_per_z_per_system[:,:r_index],
-    axis=1
-) # here we crudely sum over all redshift weights, but
-# eventually we'd like to be able to sum up to a certain redshift
-print(w_z_index.shape)
 
-# may not be able to directly mask from dcomask to get metallicities
 # fdata['BSE_System_Parameters']['Metallicity@ZAMS(1)'] has many more elements than BSE_DCO
 # we need to get the seeds from dcomask
 # then we get the metallicity from system_params in the spots where the seed is equal
@@ -94,74 +87,94 @@ stellar_types_2 = fdata['BSE_System_Parameters']['Stellar_Type@ZAMS(2)'][()][sea
 # print(set(stellar_types_1))
 # print(set(stellar_types_2))
 
-plt.figure()
 # now we would like to overlay the histogram but do it for each type of stellar object
 # ie we want to group the rates by progenitors (would be nice to do a fill on this)
 # use this as a selector Stellar_Type@ZAMS(2)
 # _, bins = np.histogram(m1zams, bins=100, density=True)
 # m1zamskde = stats.gaussian_kde(m1zams, weights=w_z_index)
 # plt.plot(bins[:-1], m1zamskde(bins[:-1]))
+def plot_up_to_redshift(ax, detector: str):
+    r_index = np.where(redshifts == sensitivity_dictionary[detector])
+    if len(r_index) == 0:
+        raise "Redshift not found..consider changing your FastCosmicIntegration settings"
+    r_index = r_index[0][0] # redshifts are monotone increasing so first result is ok
+    w_z_summed = np.sum(
+        w_per_z_per_system[:,:r_index],
+        axis=1
+    ) # eventually we'd like to be able to sum up to a certain redshift
 
-# create a subroutine that iterates through each star 1 type for now
-def plot_stellar_type_at_zams(type_index, include_histo=False):
-    bins=50
-    stellar_search = np.argwhere(stellar_types_1==type_index)
-    if include_histo:
-        plt.hist(
-            m1zams[stellar_search],
-            bins=bins,
-            weights=w_z_summed[stellar_search],
-            label=f'(1) Type {stellar_types_dictionary[type_index]}', histtype='step',
-            density=True
+    # create a subroutine that iterates through each star 1 type for now
+    def plot_stellar_type_at_zams(type_index, include_histo=False):
+        bins=1000
+        stellar_search = np.argwhere(stellar_types_1==type_index)
+        if include_histo:
+            ax.hist(
+                m1zams[stellar_search],
+                bins=bins,
+                weights=w_z_summed[stellar_search],
+                label=f'(1) Type {stellar_types_dictionary[type_index]}', histtype='step',
+                density=True
+            )
+
+        _, bins = np.histogram(m1zams[stellar_search], bins=bins)
+        # print(m1zams[stellar_search].shape)
+        # print(f'w_z_summed: {w_z_summed[stellar_search].flatten().shape}')
+        m1zamskde = stats.gaussian_kde(
+            m1zams[stellar_search].flatten(),
+            weights=w_z_summed[stellar_search].flatten()
         )
-
-    _, bins = np.histogram(m1zams[stellar_search], bins=bins)
-    # print(m1zams[stellar_search].shape)
-    # print(f'w_z_summed: {w_z_summed[stellar_search].flatten().shape}')
-    m1zamskde = stats.gaussian_kde(
-        m1zams[stellar_search].flatten(),
-        weights=w_z_summed[stellar_search].flatten()
-    )
-    plt.plot(bins[:-1]/0.012, m1zamskde(bins[:-1]), label=f'(1) Type {stellar_types_dictionary[type_index]}')
-# plt.hist(m1zams[np.argwhere(stellar_types_1==16)], bins=100, weights=w_z_index[stellar_search], legend='(1) Type 1')
-for t in set(stellar_types_1):
-    plot_stellar_type_at_zams(t)
+        ax.plot(
+            bins[:-1]/0.012, m1zamskde(bins[:-1]),
+            label=f'(1) Type {stellar_types_dictionary[type_index]} ({detector})'
+        )
+        ax.fill_between(
+            bins[:-1]/0.012,
+            m1zamskde(bins[:-1]),
+            interpolate=True,
+            alpha=0.3
+        )
+    # plt.hist(m1zams[np.argwhere(stellar_types_1==16)], bins=100, weights=w_z_index[stellar_search], legend='(1) Type 1')
+    for t in set(stellar_types_1):
+        plot_stellar_type_at_zams(t)
 # plot_stellar_type_at_zams(list(set(stellar_types_1))[0])
-plt.title(f'Number of DCO systems/year summed up to z={up_to_redshift}')
-plt.xlabel('Metallicity1 at ZAMS Z/Z0') #TODO: check units against COMPAS simulation. Looks like this is just Z
-plt.ylabel(r'Number of DCO systems/year [simulation weighted]')
-plt.xscale('log')
-plt.legend()
-plt.show()
-assert 0
+fig, ax = plt.subplots(1, 1)
+plot_up_to_redshift(ax, 'LVK')
+plot_up_to_redshift(ax, 'CE')
+
+ax.set_title(f'Number of DCO systems/year')
+ax.set_xlabel('Metallicity1 at ZAMS Z/Z0') #TODO: check units against COMPAS simulation. Looks like this is just Z
+ax.set_ylabel(r'Number of DCO systems/year [simulation weighted]')
+ax.set_xscale('log')
+ax.legend()
+fig.show()
 
 # taking from the example, we also want to be able to get the "primary black hole mass" histogram,
 # which from the examples looks like we want to get the more massive one of the two
 # will need to get the SEEDs from the DCOs that were redshifted, then select each mass
-dcomask = fdata[rate_key]['DCOmask'][()] # to be able to get seed list
-mass1 = fdata['BSE_Double_Compact_Objects']['Mass(1)'][()][dcomask]
-mass2 = fdata['BSE_Double_Compact_Objects']['Mass(2)'][()][dcomask]
-M_moreMassive = np.maximum(mass1, mass2)
+# dcomask = fdata[rate_key]['DCOmask'][()] # to be able to get seed list
+# mass1 = fdata['BSE_Double_Compact_Objects']['Mass(1)'][()][dcomask]
+# mass2 = fdata['BSE_Double_Compact_Objects']['Mass(2)'][()][dcomask]
+# M_moreMassive = np.maximum(mass1, mass2)
 
-plt.figure()
-_, bins = np.histogram(M_moreMassive, bins=100, density=True)
-plt.hist(M_moreMassive, bins=100, density=True, weights=w_z_index)
-# now do kde as well
-kde = stats.gaussian_kde(M_moreMassive, weights=w_z_index)
-
-# plt.plot(bins[:-1], counts)
-plt.plot(bins[:-1], kde(bins[:-1]))
-plt.xlabel('BH mass [Msun] ')
-plt.ylabel(r'BBH merger rate at $z =%s [\rm{Gpc}^{-3} \rm{yr}^{-1}]$'%np.round(redshifts[z_index],3), fontsize=12)
-# plt.ylabel(r'Normalized Probability')
-plt.title('BH masses of BH-BH merger population at redshift %s Density'%np.round(redshifts[z_index],3), fontsize=12)
-plt.show()
-
-plt.plot(redshifts[:-1], np.sum(w_per_z_per_system, axis=0)) # change "2" if you actually ran the weights for more redshifts (here its only for 0 and 1, ie beyond  >2)
-plt.xlabel('redshift z')
-plt.ylabel(r'BHBH merger rate at $z =%s [\rm{Gpc}^{-3} \rm{yr}^{-1}]$'%np.round(redshifts[z_index],3), fontsize=12)
-plt.title('BBH merger rate', fontsize=20)
-plt.show()
+# plt.figure()
+# _, bins = np.histogram(M_moreMassive, bins=100, density=True)
+# plt.hist(M_moreMassive, bins=100, density=True, weights=w_z_index)
+# # now do kde as well
+# kde = stats.gaussian_kde(M_moreMassive, weights=w_z_index)
+#
+# # plt.plot(bins[:-1], counts)
+# plt.plot(bins[:-1], kde(bins[:-1]))
+# plt.xlabel('BH mass [Msun] ')
+# plt.ylabel(r'BBH merger rate at $z =%s [\rm{Gpc}^{-3} \rm{yr}^{-1}]$'%np.round(redshifts[z_index],3), fontsize=12)
+# # plt.ylabel(r'Normalized Probability')
+# plt.title('BH masses of BH-BH merger population at redshift %s Density'%np.round(redshifts[z_index],3), fontsize=12)
+# plt.show()
+#
+# plt.plot(redshifts[:-1], np.sum(w_per_z_per_system, axis=0)) # change "2" if you actually ran the weights for more redshifts (here its only for 0 and 1, ie beyond  >2)
+# plt.xlabel('redshift z')
+# plt.ylabel(r'BHBH merger rate at $z =%s [\rm{Gpc}^{-3} \rm{yr}^{-1}]$'%np.round(redshifts[z_index],3), fontsize=12)
+# plt.title('BBH merger rate', fontsize=20)
+# plt.show()
 
 fdata.close()
 # then run the cosmic integrator
