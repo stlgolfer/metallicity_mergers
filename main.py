544d3ec3b012afa68f5a4d40d652a954e468654e
stlgolfer <amigala@ucsd.edu>
2025-07-30 16:16:28 -0700

    Separate dictionary

--- main.py
+++ main.py
@@ -12,26 +12,7 @@ from scipy import stats
 
 from compas_python_utils.cosmic_integration.selection_effects import SNRinterpolator
 
-stellar_types_dictionary = ["Main_Sequence_<=_0.7",
-                 "Main_Sequence_>_0.7",
-                 "Hertzsprung_Gap",
-                 "First_Giant_Branch",
-                 "Core_Helium_Burning",
-                 "Early_Asymptotic_Giant_Branch",
-                 "Thermally_Pulsing_Asymptotic_Giant_Branch",
-                 "Naked_Helium_Star_MS" ,
-             "Naked_Helium_Star_Hertzsprung_Gap" ,
-    "Naked_Helium_Star_Giant_Branch" ,
-                            "Helium_White_Dwarf" ,
-                     "Carbon-Oxygen_White_Dwarf" ,
-                       "Oxygen-Neon_White_Dwarf" ,
-                                  "Neutron_Star" ,
-                                    "Black_Hole" ,
-                              "Massless_Remnant" ,
-                        "Chemically_Homogeneous" ,
-                                          "Star" ,
-                                   "Binary_Star" ,
-    "Not_a_Star!"]
+from stellar_types import stellar_types_dictionary
 
 sensitivity_dictionary = {
     'CE': 10,
