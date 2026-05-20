6a7c18e52b9edf63a5fdfcd90bd8363a0ca8ecc0
stlgolfer <amigala@ucsd.edu>
2026-05-19 19:37:06 -0700

    Add copytest for combining merger rates

---
+++ copytest.py
@@ -0,0 +1,93 @@
+import h5py as h5
+import numpy as np
+
+def create_dummy_file():
+    #create a dummy file with some data
+    with h5.File('./dummy.h5', 'w') as fw:
+        # print(f.keys())
+        fw.create_group('Rates')
+        fw['Rates'].create_dataset('merger_rate', data=np.random.rand(20, 100))
+        fw['Rates'].create_dataset('redshifts', data=np.linspace(0, 10, num=101))
+        
+        # print(fw['test'][()])
+        print(fw['Rates'].keys())
+
+def combine_rates(rates, red_lefts, r_parity, jump):
+    # rates = np.random.randn(20, 100) # simulating 20 systems with 100 redshifts
+    # red_lefts = np.linspace(0, 10, num=101) # needs to have 100+1 to simulate left bins
+    print(rates.shape)
+    print(red_lefts)
+    # now, we need to apply a parity and combine
+    # r_parity = 2
+    z_loc = np.digitize(r_parity, red_lefts) # removing the -1 so we start collecting
+    # on the next bin. avoids double counting
+
+    # hm, maybe get the indices manually
+    new_bin_indices = []
+    j = z_loc
+    # jump = 5 # number of bins
+    while j + 5 <= len(red_lefts):
+        new_bin_indices.append(j)
+        j += jump
+    # manually add the last bin
+    new_bin_indices.append(len(red_lefts) - 1)
+    print(red_lefts[new_bin_indices])
+
+    # now we want to actually combine
+    new_red_lefts = np.concatenate((red_lefts[:z_loc], red_lefts[new_bin_indices]))
+    print(new_red_lefts.shape)
+    #hm, maybe we can grab a region, push to 3d by reshape, and then sum?
+    # print(rates[np.newaxis, :, :].reshape(5,20,20).shape) eh, just iteratively for now
+    new_rates = np.zeros((len(red_lefts[new_bin_indices])-1, 20))
+    for r in range(len(new_bin_indices) - 1):
+        start = new_bin_indices[r]
+        end = new_bin_indices[r+1]
+        block_sum = np.sum(rates[:,start:end],axis=1)
+        # print(block_sum.shape)
+        # assert 0
+        new_rates[r,:] = block_sum
+        # print(r)
+    # print(new_rates)
+    full_new_rates = np.concatenate((rates[:, :z_loc], new_rates.T), axis=1)
+    # full_new_red_lefts = np.concatenate((red_lefts[:z_loc], new_red_lefts))
+    print(full_new_rates.shape)
+    print(new_red_lefts.shape)
+    return new_red_lefts, full_new_rates
+
+if __name__ == '__main__':
+    # create_dummy_file()
+    with h5.File('./dummy.h5', 'r+') as fr:
+        new_key = 'Rates_comb'
+        if new_key in fr.keys():
+            del fr[new_key]
+
+        fr.create_group(new_key)
+        new_reds, new_rates = combine_rates(
+            fr['Rates']['merger_rate'][()],
+            fr['Rates']['redshifts'][()],
+            2, 5
+        )
+        fr[new_key].create_dataset('merger_rate', data=new_rates)
+        fr[new_key].create_dataset('redshifts', data=new_reds)
+            
+    # print('Now reading')
+    # with h5.File('./dummy.h5', 'r') as fr:
+    #     print(fr.keys())
+    # print('Now creating new group')
+    # with h5.File('./dummy.h5', 'r+') as fw: # ok so we need the r+ qualifier
+    #     if 'Rates' not in fw.keys():
+    #         fw.create_group('Rates')
+    #     if 'yuh' not in fw['Rates'].keys():
+    #         fw['Rates'].create_dataset('yuh', data=np.random.randn(10))
+    #     print(fw['Rates'])
+
+    # now make a dummy dataset
+    
+    
+    # now grab the large
+    # blocked = rates[:,z_loc:]
+    # course_red_lefts = red_lefts[z_loc:]
+    # splitted = np.array_split(course_red_lefts, 5)
+    # for i in range(len(splitted)):
+    #     splitted[i] = np.array([splitted[i][0],splitted[i][-1]])
+    # print(splitted) # this is the list of intervals for the new bins
